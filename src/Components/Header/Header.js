2804c45fc4b49d75cf1d8ee67ed683fa263b141e
Elvira2996 <alikova@iksu.kg>
2024-06-04 05:51:50 +0300

    to added sectoin header footer

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import './Header.css';
+import Menu from '../Menu/Menu';
+import Button from '../Button/Button';
+import { Link } from 'react-router-dom';
+import CategoryList from '../CategoryList/CategoryList';
+
+
+function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+    
+      <header className={`Header ${isScrolled ? 'fixed' : ''}`}>
+        <div className="container">
+          <div className="Head flex">
+            <h1 className="logo"> <Link to="#">Versace</Link></h1>
+            <nav>
+              <CategoryList />
+              
+              {/* <ul className='header_menu flex'>
+                <li><Link to="#">La Vacanza</Link></li>
+                <li><Link to="#">New In</Link></li>
+                <li><Link to="#">Bags</Link></li>
+                <li><Link to="#">Woman</Link></li>
+                <li><Link to="#">Men</Link></li>
+                <li><Link to="#">Children</Link></li>
+                <li><Link to="#">Stories</Link></li>
+              </ul> */}
+            </nav>
+            <div className="icons">
+              <span className='ic_menu'></span>
+              <span className='ic_menu'></span>
+              <span className='ic_menu'></span>
+            </div>
+            <Menu />
+            <div className="Buttons flex">
+              <Button>Login</Button>
+              <Button>Sign In</Button>
+            </div>
+            <Link to="/cart" className="Cart_ic">
+              <span></span>
+            </Link>
+          </div>
+        </div>
+        
+      </header>
+      
+    </>
+  );
+}
+
+export default Header;
