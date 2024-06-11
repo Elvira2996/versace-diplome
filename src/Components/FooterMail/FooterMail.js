b61fd368a07ac1508bde7cd652ac34404fc89ba7
Elvira2996 <alikova@iksu.kg>
2024-06-11 06:26:01 +0300

    to add input

--- src/Components/FooterMail/FooterMail.js
+++ src/Components/FooterMail/FooterMail.js
@@ -1,23 +1,41 @@
+import React, { useState } from 'react';
 import './FooterMail.css';
 
 function FooterMail() {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Предотвращаем стандартное поведение отправки формы
+
+    // Ваш код для отправки письма
+    // Например, здесь может быть AJAX-запрос к серверу для отправки письма
+
+    alert("Письмо отправлено на адрес: " + email); // Вместо этого вы можете использовать реальную отправку письма
+    
+    // Очистка поля ввода
+    setEmail('');
+  };
+
   return (
-    <>
-        <section className='enter'>
-            <div className='container'>
-              <h2 className='enter_title'>ENTER THE WORLD OF VERSACE</h2>
-              <p className='enter_txt'>Stay up to date on events, collections and exclusive news
-              </p>
-              <form className='enter_mail'>
-             
-                
-                <input className='form_input' type="email" placeholder='Enter your email address' />
-                  <div className='enter_ic'></div>
-                
-              </form>
-            </div>
-          </section>
-    </>
+    <section className='enter'>
+      <div className='container'>
+        <h2 className='enter_title'>ENTER THE WORLD OF VERSACE</h2>
+        <p className='enter_txt'>Stay up to date on events, collections and exclusive news</p>
+        <form className="email-form" onSubmit={handleSubmit}>
+          <div className="email-input">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} // Обновляем значение email при изменении ввода
+            />
+            <button type="submit">
+              <span>&#8594;</span> {/* Стрелка вправо */}
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
   );
 }
 
